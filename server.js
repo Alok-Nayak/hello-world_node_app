620ea5e39e0cf7e91acad46b64f3b81a2395fc19
aloknayak <56588590+Alok-Nayak@users.noreply.github.com>
2024-01-02 12:34:16 +0530

    sample app

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+// Constants
+const PORT = process.env.PORT || 80;
+const HOST = "0.0.0.0";
+
+// App
+const app = express();
+
+// Health Check Endpoint
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+// Main Endpoint
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Start the Server
+let server = app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
+
+module.exports = server;
